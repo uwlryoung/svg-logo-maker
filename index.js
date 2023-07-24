a29f0971f4060b46a01b666fa2f461ef8050d175
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-07-23 21:38:09 -0400

    Updated the run function to have a welcome message. Added an index.html file so that users could open the file in the browser. Made 'logoFolder' where the .svg folder will be saved.

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const fs = require('fs');
 const {Circle, Square, Triangle} = require('./lib/logo.js');
 
 function run() {
+  console.log(`
+  _                           _______                                                
+ (_)                         (_______)                                _              
+  _       ___   ____  ___     _   ___ _____ ____  _____  ____ _____ _| |_ ___   ____ 
+ | |     / _ \\ / _  |/ _ \\   | | (_  | ___ |  _ \\| ___ |/ ___|____ (_   _) _ \\ / ___)
+ | |____| |_| ( (_| | |_| |  | |___) | ____| | | | ____| |   / ___ | | || |_| | |    
+ |_______)___/ \\___ |\\___/    \\_____/|_____)_| |_|_____)_|   \\_____|  \\__)___/|_|    
+              (_____| `)
+
+  console.log("Thank you for using our Logo Generator. Make your very own logo as an .svg file!\nPlease answer the following prompts to make one:\n");
+
   inquirer
     .prompt([
       {
@@ -11,10 +22,10 @@ function run() {
         message: '\nPlease enter up to 3 charaters to be displayed on your logo: ',
         validate: function(characters){
           if (characters.length > 3){
-            console.log("\nYour logo must have 3 or fewer characters in it. Please try again.\n");
+            console.log("\n\x1b[31mYour logo must have 3 or fewer characters in it. Please try again.\n");
             return false;
           } else if (characters.length === 0) {
-            console.log("\nYour logo must have at least one character in it. Please try again.\n")
+            console.log("\n\x1b[31mYour logo must have at least one character in it. Please try again.\n")
             return false;
           } else {
             return true;
@@ -35,17 +46,23 @@ function run() {
       {
         type: 'input',
         name: 'shapeColor',
-        message: '\nWhat \x1b[36mcolor\x1b[0m would you like the shape to be? \x1b[90m(Color name or Hexadecimal number): '
+        message: '\nWhat \x1b[36mcolor\x1b[0m would you like the shape to be? \x1b[90m(Color name or Hexadecimal number): ',
       }
     ])
     .then(function(response) {
+      const folderName = './logoFolder';
+      // Creates a folder to put the readme file into
+		  if (!fs.existsSync(folderName)) {
+			fs.mkdirSync(folderName);
+		  } 
+
       if (response.shapes === "circle"){
         const shape = new Circle(
           response.characters,
           response.textColor,
           response.shapeColor
         )
-        fs.writeFile('logo.svg', shape.render(), (err) =>
+        fs.writeFile(`${folderName}/logo.svg`, shape.render(), (err) =>
           err ? console.error(err) : console.log('\nSuccess!')
         )
       } else if (response.shapes === "square") {
@@ -54,7 +71,7 @@ function run() {
           response.textColor,
           response.shapeColor
         )
-        fs.writeFile('logo.svg', shape.render(), (err) =>
+        fs.writeFile(`${folderName}/logo.svg`, shape.render(), (err) =>
           err ? console.error(err) : console.log('Success!')
         )
       } else if (response.shapes === "triangle") {
@@ -63,8 +80,8 @@ function run() {
           response.textColor,
           response.shapeColor
         )
-        fs.writeFile('logo.svg', shape.render(), (err) =>
-          err ? console.error(err) : console.log('Success!')
+        fs.writeFile(`${folderName}/logo.svg`, shape.render(), (err) =>
+          err ? console.error(err) : console.log('Generated logo.svg! You can find your logo in the "logoFolder".')
         )
       }
     })
